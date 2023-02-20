55b9630c2b654b9269423f489a4553262bd850e9
peterpan0225 <wasyl.0225@gmail.com>
2023-02-21 00:22:15 +0200

    added TodoList

---
+++ src/ToDoInputForm.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { toDoItems } from "./store.js"
+  import { onMount } from "svelte";
+  export let userName
+  let newItem = ""
+  let newItemInputFrom = null
+
+  onMount(() => {
+    newItemInputFrom.focus()
+  })
+  
+  function addToList() {
+    $toDoItems = [...$toDoItems, {text: newItem, status: false}]
+    newItem = ""
+  }
+</script>
+  
+<input bind:value={newItem} bind:this={newItemInputFrom} type="text" placeholder="Add a new task">
+<button on:click={addToList}>Add</button>
+
+<h3>{userName}'s task list:</h3>
